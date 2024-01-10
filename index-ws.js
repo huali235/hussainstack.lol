861943bc8a6928904ba4340400942c9f0ae6d229
Hussain Ali <huali235@gmail.com>
2024-01-09 22:10:33 -0500

    add database logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ server.listen(3000, function () {
   console.log("Listening on 3000");
 });
 
+process.on("SIGINT", () => {
+  console.log("sigint");
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /** Begin Websocket */
 
 const WebSocketServer = require("ws").Server;
@@ -25,6 +35,10 @@ wss.on("connection", function (ws) {
 
   if (ws.readyState === ws.OPEN) ws.send("Welcome to my server");
 
+  db.run(`INSERT INTO visitors (count, time)
+      VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on("close", function close() {
     console.log("A client has disconnected");
   });
@@ -35,3 +49,31 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** End Websocket */
+
+/** Begin Database */
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+      CREATE TABLE visitors (
+        count INTEGER,
+        time TEXT
+      )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down db");
+  db.close();
+}
